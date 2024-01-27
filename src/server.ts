7cd9a7964e3568fca36f51640af57ee87d8cf51c
joao-code-web <joaogabriel9512@gmail.com>
2024-01-27 15:40:12 -0300

    ota

--- src/server.ts
+++ src/server.ts
@@ -10,13 +10,16 @@ const start = async () => {
 
     await app.register(routes)
     await app.register(cors)
+    const port: number = parseInt(process.env.PORT || '3000', 10);
 
     try {
-        await app.listen({ port: process.env.PORT || 3000 })
-        console.log("almslakso")
+        await app.listen(port);
+        console.log(`Servidor iniciado na porta ${port}`);
     } catch (error) {
+        console.error("Erro ao iniciar o servidor:", error);
         process.exit(1);
     }
+
 }
 
 start()
